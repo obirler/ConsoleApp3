ac8784054a255b65f950ade7c487b7ad54fb5cee
obirler <omer.birler@outlook.com>
2022-11-27 12:46:35 +0300

    Add project files.

---
+++ ConsoleApp3/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+            Area = 2 * Math.PI * Radius * Radius;
+            Perimeter = 2 * Math.PI * Radius;
+        }
+
+        public double Radius;
+    }
+}
---
+++ ConsoleApp3/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Rectangle : Shape
+    {
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+            Area = Height * Width;
+            Perimeter = 2 * (Height + Width);
+        }
+
+        public double Height;
+
+        public double Width;
+    }
+}
